6b615e1dacfb212786ff9035d5f0616a55c22941
Nastya Stepchenkova <anstepchen@icloud.com>
2017-02-13 16:42:30 +0300

    new

---
+++ AccountStateHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcLibrary
+{
+    public delegate void AccountStateHandler(object sender, AccountEventArgs e);
+
+    public class AccountEventArgs
+    {
+        //the message about the event
+        public string Message { get; private set; }
+        //the result of operations
+        public decimal Expression { get; private set; }
+
+        public AccountEventArgs(string _mes, decimal _expression)
+        {
+            Message = _mes;
+            Expression = _expression;
+        }
+    }
+}
---
+++ IAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcLibrary
+{
+    interface IAccount
+    {
+        //add new number to expression
+        void Adding(decimal expression);
+        //subtract new number from expression
+        decimal Subtracted(decimal expression);
+        //multiply new number to expression
+        decimal Multiplyed(decimal expression);
+        //divide the expression on new number
+        decimal Divided(decimal expression);
+
+    }
+}
